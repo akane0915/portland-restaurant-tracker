bea729457ac1f957a35f5ee461a1893bb2cd5acf
akane0915 <asialkane@gmail.com>
2017-05-08 21:06:34 -0700

    add routing for restaurants page

--- app.rb
+++ app.rb
@@ -66,58 +66,25 @@ delete'/locations/:id' do
 end
 
 
-get '/cuisines/:id' do
-  id = params.fetch('id').to_i
-  @cuisine = Cuisine.find(id)
-  erb :cuisine
+
+
+
+get '/restaurants' do
+  @restaurants = Restaurant.all
+  erb :restaurants
 end
 
-delete'/cuisines/:id' do
-  id = params.fetch('id').to_i
-  @cuisine = Cuisine.find(id)
-  @cuisine.delete
-  redirect '/cuisines'
+get '/restaurants/new' do
+  @cuisines = Cuisine.all
+  @locations = Location.all
+  erb :restaurant_form
 end
-
-
-
-
 
 post '/restaurants' do
   name = params.fetch('name')
   price = params.fetch('price').to_i
   cuisine_id = params.fetch('cuisine_id').to_i
-  new_restaurant = Restaurant.create({:name => name, :price => price, :cuisine_id => cuisine_id})
-  @restaurant = Restaurant.find(cuisine_id)
-  erb :restaurant
-end
-
-
-
-
-
-get '/restaurants/:id/edit' do
-  id = params.fetch('id').to_i
-  @restaurant = Restaurant.find(id)
-  erb :restaurant_edit
-end
-
-patch '/restaurants/:id' do
-  name = params.fetch('name')
-  price = params.fetch('price').to_i
-  id = params.fetch('id').to_i
-  @restaurant = Restaurant.find(id)
-  @restaurant.update({:name => name, :price => price})
-  cuisine_id = @restaurant.cuisine_id
-  @cuisine = Restaurant.find(cuisine_id)
-  erb :cuisine
-end
-
-delete '/restaurants/:id' do
-  id = params.fetch('id').to_i
-  @restaurant = Restaurant.find(id)
-  cuisine_id = @restaurant.cuisine_id
-  @cuisine = Restaurant.find(cuisine_id)
-  @restaurant.delete
-  erb :cuisine
+  location_id = params.fetch('location_id').to_i
+  new_restaurant = Restaurant.create({:name => name, :price => price, :cuisine_id => cuisine_id, :location_id => location_id})
+  redirect '/restaurants'
 end
