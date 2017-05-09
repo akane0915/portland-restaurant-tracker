67d987d78706961fc7619cee957af9011c8ea4d0
akane0915 <asialkane@gmail.com>
2017-05-08 20:51:22 -0700

    add passing test and method to return the location of a restaurant

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -9,4 +9,12 @@ describe Restaurant do
       expect(test_restaurant.cuisine).to eq test_cuisine
     end
   end
+
+  describe'#location' do
+    it 'tells which location it is in' do
+      test_location = Location.create({:neighborhood => "Northwest"})
+      test_restaurant = Restaurant.create({:name => "restaurant", :location_id => test_location.id})
+      expect(test_restaurant.location).to eq test_location
+    end
+  end
 end
